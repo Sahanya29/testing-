b1efbe2e48e1392884f73e5065605e3eaa2c3fac
unknown <email>
2021-05-13 11:08:16 +0530

    first commit

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent  {
+
+  products=[1,2,3,4,5];
+
+}
